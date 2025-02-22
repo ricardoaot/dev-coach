984be20acdeeb21b754bebc73ad9973c520039b9
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2025-02-21 19:56:06 -0500

    suffledoptions

--- src/components/molecules/QuestionForm.tsx
+++ src/components/molecules/QuestionForm.tsx
@@ -2,12 +2,17 @@ import { RadioInput } from "../atoms/inputs/RadioInput";
 import ReactMarkdown from "react-markdown";
 import MarkdownComponents from "../../helpers/MarkdownComponents";
 import { QuestionFormContentProps } from "../../interfaces/interfaces";
+import { useMemo } from "react";
 
 export const QuestionFormContent: React.FC<QuestionFormContentProps> = ({
   questionData,
   selectedOption,
   handleOptionChange,
 }) => {
+    // Mezclar las opciones aleatoriamente con useMemo para evitar remezclado en cada render
+    const shuffledOptions = useMemo(() => {
+      return [...questionData.answerOptions].sort(() => Math.random() - 0.5);
+    }, [questionData]);
   return (
     <>
       <h1 className="mb-4 text-xl font-bold">
@@ -16,7 +21,7 @@ export const QuestionFormContent: React.FC<QuestionFormContentProps> = ({
         </ReactMarkdown>
         <hr />
       </h1>
-      {questionData.answerOptions.map((option, index) => (
+      {shuffledOptions.map((option, index) => (
         <div key={index} className="flex items-start gap-2 mb-2">
           <div className="mt-1.5">
             <RadioInput
